2d45fd79e1512503afa11742ec4fd522dcf2c8bd
shadolproff <shadolproff420@mail.ru>
2022-08-06 23:03:43 +0300

    Инициализация (прототип + тех. детали)

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ t.cpp -std=c++23 -lfmt -fsanitize=undefined,address,leak -Wall -Wextra -Wconversion -Wpedantic && ./a.out
---
+++ vcae_util.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+static_assert(sizeof(long) == 8,
+              "Для i64 нужен 8-байтный целочисленный тип данных");
+using i64 = long;
+static_assert(sizeof(double) == 8,
+              "Для f64 нужен 8-байтный тип данных c плавающей точкой");
+using f64 = double;
+static_assert(sizeof(signed char) == 1,
+              "Для byte нужен 1-байтный целочисленный тип данных");
+using byte = signed char;
