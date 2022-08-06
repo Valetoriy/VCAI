6713592130c13d060db6a35a7ab35fe6cf7b9858
shadolproff <shadolproff420@mail.ru>
2022-08-06 23:15:11 +0300

    Реструктурирование проекта

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ t.cpp -std=c++23 -lfmt -fsanitize=undefined,address,leak -Wall -Wextra -Wconversion -Wpedantic && ./a.out
+g++ src/t.cpp -o VCAE -std=c++23 -lfmt -fsanitize=undefined,address,leak -Wall -Wextra -Wconversion -Wpedantic
